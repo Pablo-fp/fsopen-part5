d18c7f733380aa8e88ec7de691e06f442859b21f
Pablo-fp <pablofp1988@hotmail.com>
2025-01-31 19:54:44 +0100

    5.17: Blog List End To End Testing, step 1

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,9 +1,9 @@
-describe('Note app', function () {
-  it('front page can be opened', function () {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5173');
-    cy.contains('Notes');
-    cy.contains(
-      'Note app, Department of Computer Science, University of Helsinki 2023'
-    );
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('login');
   });
 });
